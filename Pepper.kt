da2c6b8f4d76c83015209b457367587a402c7b5d
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 15:19:20 +0100

    Range 1

--- Pepper.kt
+++ Pepper.kt
@@ -2,8 +2,19 @@ fun main() {
     var sHU = 17000
     var pungency: String
 
-    // Write your code below
+    // Determine the pungency based on the SHU value
+    if (sHU in 0..699) {
+        pungency = "very mild"
+    } else if (sHU in 700..2999) {
+        pungency = "mild"
+    } else if (sHU in 3000..24999) {
+        pungency = "moderate"
+    } else if (sHU in 25000..69999) {
+        pungency = "high"
+    } else {
+        pungency = "very high"
+    }
 
-
-
+    // Output the pungency level
+    println("Pungency: $pungency")
 }
