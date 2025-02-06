40ae253d1fe2c64980bd398fe942a875d6936049
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 15:30:43 +0100

    Creating a Mutable List 2

--- BroadWay.kt
+++ BroadWay.kt
@@ -2,5 +2,6 @@ fun main() {
     // Creating a mutable list of Broadway shows
     val openBroadwayShows = mutableListOf("The Lion King","Chicago","The Cher Show","Les Miserables"
     )
+    openBroadwayShows[2] = "Wicked"
     println(openBroadwayShows)
 }
