1671668da93f7011fadc998e2207b60e4ab8ca6d
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 15:23:54 +0100

    Creating an Immutable List

--- Sports.kt
+++ Sports.kt
@@ -1,5 +1,13 @@
 fun main() {
-    // Write your code below 🏊‍♀️
+    // Creating an immutable list of water sports
+    val waterSports = listOf(
+            "Wind Surfing",
+            "Sailing",
+            "Swimming",
+            "Jet Skiing",
+            "Water Skiing"
+    )
 
-
+    // Optionally, print the list to verify its contents
+    println(waterSports)
 }
