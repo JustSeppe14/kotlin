7eb647442d2b918a3f56ea870896cfca35b40f6d
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 15:25:29 +0100

    Accessing Elements in a List 1

---
+++ Marathon.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Creating an immutable list of popular U.S. marathons
+    val unitedStatesMarathons = listOf(
+            "Boston Marathon",
+            "Bank of America Chicago Marathon",
+            "TCS NYC Marathon",
+            "Marine Corps Marathon"
+    )
+}
