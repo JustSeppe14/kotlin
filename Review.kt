53391862370d2a490e4c90ed8b248792b0115eb6
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 14:04:47 +0100

    Review

--- Review.kt
+++ Review.kt
@@ -1,7 +1,17 @@
-// Add your code below
+// Lesson 1
+/*
 fun main(){
     // Print age
     print(2025-2003)
     // Print favorite quote
     println("It is nice to be important, but it's more important to be nice.")
+}
+*/
+// Lesson 2
+fun main() {
+    val favoriteSong = "Bohemian Rhapsody"
+    val songDuration = 6
+    var isListening = true
+
+    println("$favoriteSong is $songDuration minutes long. Listening now? $isListening")
 }
