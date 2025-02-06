c3e74040eccb636714c6bd13de977a80d5a11b47
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 13:52:06 +0100

    Optional

--- Art.kt
+++ Art.kt
@@ -3,8 +3,24 @@
 ( o.o )
  > ^ <
 */
-fun main(){
+/*fun main(){
     println(" /\\_/\\  ")
     println("( o.o ) ")
     println(" > ^ < ")
+}
+*/
+// Optional
+/*
+fun main() {
+    println(" /\\_/\\  \n( o.o ) \n > ^ < ")
+}
+*/
+
+// Background
+fun main() {
+    println("********")
+    println("* /\\_/\\ *")
+    println("*( o.o )*")
+    println("* > ^ < *")
+    println("********")
 }
