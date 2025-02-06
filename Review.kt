9eac2d686005f835d91ef5318ffda61eb27708bd
Seppe Van Campfort <r0929446@student.thomasmore.be>
2025-02-06 13:46:18 +0100

    End first lesson

---
+++ Review.kt
@@ -0,0 +1,7 @@
+// Add your code below
+fun main(){
+    // Print age
+    print(2025-2003)
+    // Print favorite quote
+    println("It is nice to be important, but it's more important to be nice.")
+}
